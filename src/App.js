d830cdf05cab4dad5e2c31458651762b33c13a20
Alexander Veselov <collabsis.g@gmail.com>
2023-11-03 10:58:20 +0400

    Updated savecsv controller, but he has a bug =)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
-import React, {useState, useEffect} from'react';
-import { Container,  Row, Col } from'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import ky from "ky";
-import Filter from './components/Filter/Filter';
-import ChartComponent from './components/Chart/ChartComponent';
-import ListComponent from './components/List/ListComponent';
-import NavBarComponent from './components/Navbar/NavBarComponent';
-import CsvDownloaderComponent from './components/CsvDownloader/CsvDownloaderComponent';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/scrollable-element.css'
+import Filter from "./components/Filter/Filter";
+import ChartComponent from "./components/Chart/ChartComponent";
+import ListComponent from "./components/List/ListComponent";
+import NavBarComponent from "./components/Navbar/NavBarComponent";
+import CsvDownloaderComponent from "./components/CsvDownloader/CsvDownloaderComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/scrollable-element.css";
 
 function App() {
   const [data, setData] = useState([]);
-  const [filter, setFilter] = useState('');
-  const [chartData, setChartData] = useState([])
-  const [selectedPoint, setSelectedPoint] = useState('');
+  const [filter, setFilter] = useState("");
+  const [chartData, setChartData] = useState([]);
+  const [selectedPoint, setSelectedPoint] = useState("");
+  const [reportRows, setReportRows] = useState([]);
+  const [reportColumns, setReportColumns] = useState([]);
+  const [isReportLoading, setIsReportLoading] = useState(false);
 
   async function getData() {
-    const result = await ky.get('items', { prefixUrl: process.env.REACT_APP_API_HOST }).json();
+    const result = await ky
+      .get("items", { prefixUrl: process.env.REACT_APP_API_HOST })
+      .json();
     setData(result);
   }
 
   async function getChartData(id) {
-    const result = await ky.get(`data-raw/daily/${id}`, { prefixUrl: process.env.REACT_APP_API_HOST }).json();
+    const result = await ky
+      .get(`data-raw/daily/${id}`, {
+        prefixUrl: process.env.REACT_APP_API_HOST,
+      })
+      .json();
     setChartData(result);
   }
 
   useEffect(() => {
     getData();
   }, []);
+
+  useEffect(() => {
+    if (reportRows.length > 1) {
+      setIsReportLoading(false);
+      console.log(reportRows);
+    }
+  }, [reportRows]);
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
@@ -38,9 +54,55 @@ function App() {
     setSelectedPoint(event.target.textContent);
   };
 
-  const filteredData = data.filter(
-    (item) =>
-      item.name.toLowerCase().includes(filter.toLowerCase())
+  const handleReportClick = async () => {
+    setIsReportLoading(true);
+    try {
+      await ky
+        .get(`data-raw/report/daily/2`, {
+          prefixUrl: process.env.REACT_APP_API_HOST,
+        })
+        .json()
+        .then((response) => {
+          const headers = [
+            {
+              id: "name",
+              displayName: "Т. контроля",
+            },
+            {
+              id: "layer",
+              displayName: "Слой",
+            },
+            {
+              id: "source_time_date",
+              displayName: "Дата",
+            },
+            {
+              id: "source_time_time",
+              displayName: "Время",
+            },
+            {
+              id: "value",
+              displayName: "Значение",
+            },
+          ];
+
+          const rows = response.map((item) => [
+            item.name,
+            item.layer,
+            new Date(item.source_time).toLocaleString("en-GB"),
+            item.value,
+          ]);
+
+          setReportRows(rows);
+          setReportColumns(headers);
+        });
+    } catch (error) {
+      console.error("Ошибка при загрузке данных", error);
+    }
+  };
+
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(filter.toLowerCase())
   );
 
   return (
@@ -50,22 +112,34 @@ function App() {
           <NavBarComponent />
         </Row>
         <Row>
-          <Col xs={3} className='ps-0' >
-            {/* <Row>
-              <CsvDownloaderComponent  />
-            </Row> */}
+          <Col xs={3} className="ps-0">
             <Row>
-              <Filter className="ps-0 pe-0" filter={filter} onFilterChange={handleFilterChange} />
+              <CsvDownloaderComponent
+                datas={reportRows}
+                columns={reportColumns}
+                isLoading={isReportLoading}
+                onClick={handleReportClick}
+              />
             </Row>
             <Row>
-              <Container  className='scrollable-element' style={{ height: "80vh", overflow: 'auto'}} >
+              <Filter
+                className="ps-0 pe-0"
+                filter={filter}
+                onFilterChange={handleFilterChange}
+              />
+            </Row>
+            <Row>
+              <Container
+                className="scrollable-element"
+                style={{ height: "73vh", overflow: "auto" }}
+              >
                 <ListComponent data={filteredData} onClick={handleListClick} />
               </Container>
             </Row>
           </Col>
 
-          <Col xs={9} className='pe-0'>
-            <ChartComponent chartData={chartData} pointName={selectedPoint}/>
+          <Col xs={9} className="pe-0">
+            <ChartComponent chartData={chartData} pointName={selectedPoint} />
           </Col>
         </Row>
 
--- src/components/CsvDownloader/CsvDownloaderComponent.jsx
+++ src/components/CsvDownloader/CsvDownloaderComponent.jsx
@@ -1,84 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import CsvDownloader from "react-csv-downloader";
-import ky from "ky";
 import { Button, Spinner } from "react-bootstrap";
 
-export default function CsvDownloaderComponent() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [datas, setDatas] = useState([]);
-  const [columns, setColumns] = useState([]);
-
-  const handleReportClick = async () => {
-    setIsLoading(true);
-    try {
-      const result = await ky
-        .get(`data-raw/report/daily/2`, {
-          prefixUrl: process.env.REACT_APP_API_HOST,
-        })
-        .json();
-
-        console.log("Полученные данные:", result); 
-      const headers = [
-        {
-          id: "name",
-          displayName: "Т. контроля",
-        },
-        {
-          id: "layer",
-          displayName: "Слой",
-        },
-        {
-          id: "source_time",
-          displayName: "Время",
-        },
-        {
-          id: "value",
-          displayName: "Значение",
-        },
-      ];
-
-      const rows = result.map((item) => [
-        {
-          name: item.name,
-          layer: item.layer,
-          source_time: item.source_time,
-          value: item.value,
-        },
-      ]);
-
-      console.table(headers);
-      console.table(rows);
-      setDatas(rows);
-      setColumns(headers);
-    } catch (error) {
-      console.error("Ошибка при загрузке данных", error);
-    }
-
-    console.log(datas);
-    console.log(columns);
-    setIsLoading(false);
-  };
-
+export default function CsvDownloaderComponent({datas, columns, isLoading, onClick }) {
   return (
     <>
-      {isLoading ? (
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      ) : (
         <CsvDownloader
-          filename={new Date().toString()}
+          filename={new Date().toLocaleString("en-GB")}
           extension=".csv"
           separator=";"
-          wrapColumnChar="'"
           columns={columns}
           datas={datas}
         >
-          <Button disabled={isLoading} onClick={handleReportClick}>
-            Download CSV
+          <Button className="bg-light border-0 text-black w-100 mb-2" onClick={onClick}>
+            {isLoading ? (
+            <Spinner animation="border" role="status" style={{height: "18px", width: "18px"}} />
+          ) : ('Скачать отчет')}
           </Button>
         </CsvDownloader>
-      )}
     </>
   );
 }
